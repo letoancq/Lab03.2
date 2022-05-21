4e0f233760dd42ff76833085ca4bff163df892f3
ToanFX15903 <vtv5hdcq@gmail.com>
2022-05-21 23:29:23 +0700

    post file

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -74,11 +74,7 @@ function Footer(props) {
             </div>
           </div>
         </div>
-        <div className="row justify-content-center">
-          <div className="col-auto">
-            <p>© Copyright 2018 Ristorante Con Fusion</p>
-          </div>
-        </div>
+        
       </div>
     </div>
   );
--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -1,8 +1,8 @@
 import React from "react";
 import dateFormat from "dateformat";
 import { STAFFS } from "../shared/staffs";
-
-
+import { Link } from "react-router-dom";
+import { Breadcrumb, BreadcrumbItem } from "reactstrap";
 
 class StaffList extends React.Component {
   constructor(props) {
@@ -17,14 +17,21 @@ class StaffList extends React.Component {
   renderInfo = (info) => {
     if (info != null) {
       return (
-        <div className="form-info" key={info.id}>
-          <h2>Họ và tên : {info.name}</h2>
-          <p>Ngày sinh : {dateFormat(info.doB, "dd/mm/yyyy")}</p>
-          <p>Ngày vào công ty : {dateFormat(info.startDate, "dd/mm/yyyy")}</p>
-          <p>Phòng ban : {info.department.name}</p>
-          <p>Số ngày nghỉ còn lại :  {info.annualLeave}</p>
-          <p>Số ngày đã làm thêm :  {info.overTime}</p>
-          
+        <div className="container">
+          <Breadcrumb>
+            <BreadcrumbItem>
+              <Link to="/home">Home</Link>
+            </BreadcrumbItem>
+            <BreadcrumbItem active>{info.name}</BreadcrumbItem>
+          </Breadcrumb>
+          <div className="form-info" key={info.id}>
+            <h2>Họ và tên : {info.name}</h2>
+            <p>Ngày sinh : {dateFormat(info.doB, "dd/mm/yyyy")}</p>
+            <p>Ngày vào công ty : {dateFormat(info.startDate, "dd/mm/yyyy")}</p>
+            <p>Phòng ban : {info.department.name}</p>
+            <p>Số ngày nghỉ còn lại : {info.annualLeave}</p>
+            <p>Số ngày đã làm thêm : {info.overTime}</p>
+          </div>
         </div>
       );
     } else {
@@ -34,7 +41,6 @@ class StaffList extends React.Component {
   render() {
     const list = STAFFS.map((abc) => {
       return (
-        
         <span className="span-list" onClick={() => this.onClick(abc)}>
           <div key={abc.id} className="col">
             <img src={abc.image} alt={abc.name} />
@@ -45,11 +51,9 @@ class StaffList extends React.Component {
     });
     return (
       <div className="container-staff">
-            <h3 className="list-staffs">Danh sách nhân viên</h3>
-
+        <h3 className="list-staffs">Danh sách nhân viên</h3>
         <div className="staff">{list}</div>;
         <div className="info">{this.renderInfo(this.state.selectedInfo)}</div>
-
       </div>
     );
   }
